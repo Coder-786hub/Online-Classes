c7da38d4c39c0a91434a7579f1da50863ed9121b
Aftab Alam <aftabalamkhan8932@gmail.com>
2024-11-05 09:24:06 +0530

    Commited

---
+++ 9 to 10/05-11.py
@@ -0,0 +1,31 @@
+# Transfer Statement
+
+
+
+# Break 
+# continue
+# pass
+
+# Break 
+# for i in range(1,10):
+#     print(i)
+#     if i == 5:
+#         break
+    # print()
+
+# continue
+# for i in range(1, 10):
+#     if i == 8:
+#         continue
+#     print(i)
+
+# pass    
+
+# for i in range(1, 10):
+#     if i == 8:
+#         pass
+#     print(i)
+
+
+def fun():
+    pass
